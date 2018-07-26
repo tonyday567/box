4348256ae51cf21fbc852a22aa4750f4c27b0fa1
Tony Day <tonyday567@gmail.com>
2018-07-26 12:52:47 +1000

    Changes needed for websocket-box

---
+++ src/Box/Control.hs
@@ -0,0 +1,154 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# OPTIONS_GHC -Wall #-}
+
+module Box.Control
+  ( ControlComm(..)
+  -- , ControlComm(..)
+  , ControlBox
+  , ControlConfig(..)
+  , consoleControlBox
+  , parseControlComms
+  , controlBox
+  , runControlBox
+  , testBox
+  , timeOut
+  ) where
+
+import Box
+import Control.Applicative
+import Control.Category
+import Control.Concurrent.Async
+import Control.Lens hiding ((|>))
+import Control.Monad
+import Data.Data
+import Data.Default
+import Data.IORef
+import Flow
+import GHC.Generics
+import Protolude hiding ((.), STM)
+import Text.Read (readMaybe)
+import qualified Data.Attoparsec.Text as A
+import qualified Data.Text as Text
+import qualified Streaming.Prelude as S
+import Control.Monad.Conc.Class as C
+
+data ControlComm
+  = Ready -- ready for comms
+  | Check -- check for existence
+  | Died -- died (of its own accord)
+  | Stop -- stop (without shutting down)
+  | Kill -- stop and quit (& cancel thread)
+  | ShutDown -- successfully Killed
+  | Start -- start (if not yet started)
+  | Reset -- stop and start (potentially cancelling a previous instance)
+  | On Bool -- are we live?
+  | Log Text
+  deriving (Show, Read, Eq, Data, Typeable, Generic)
+
+type ControlBox m = (MonadConc m) => Cont m (Box (STM m) ControlComm ControlComm)
+
+data ControlConfig
+  = KeepAlive Double
+  | AllowDeath
+  deriving (Show, Eq)
+
+instance Default ControlConfig where
+  def = AllowDeath
+
+consoleControlBox :: ControlBox IO
+consoleControlBox =
+  Box <$>
+  (contramap show <$>
+   (cStdout 1000 :: Cont IO (Committer (STM IO) Text))) <*>
+  (emap (pure . either (const Nothing) Just) <$>
+   (eParse parseControlComms <$>
+    eStdin 1000))
+
+parseControlComms :: A.Parser ControlComm
+parseControlComms =
+  A.string "q" *> return Stop <|> A.string "s" *> return Start <|>
+  A.string "x" *> return Kill <|> do
+    res <- readMaybe . Text.unpack <$> A.takeText
+    case res of
+      Nothing -> mzero
+      Just a -> return a
+
+-- | an effect that can be started and stopped
+-- committer is an existence test
+-- controlBox :: (MonadConc m) => ControlConfig -> m () -> ControlBox m
+controlBox
+  :: ControlConfig
+     -> IO a -> Box (STM IO) ControlComm ControlComm -> IO Bool
+controlBox cfg app (Box c e) = do
+  ref' <- newIORef Nothing
+  go ref'
+  where
+    go ref = do
+      msg <- C.atomically $ emit e
+      case msg of
+        Nothing -> go ref
+        Just msg' ->
+          case msg' of
+            Check -> do
+              a <- readIORef ref
+              _ <-
+                C.atomically $ commit c $ On (bool True False (isNothing a))
+              go ref
+            Start -> do
+              a <- readIORef ref
+              when (isNothing a) (void $ start ref c)
+              go ref
+            Stop -> cancel' ref >> go ref
+            Kill -> cancel' ref >> C.atomically (commit c ShutDown)
+            Died ->
+              case cfg of
+                AllowDeath -> C.atomically $ commit c ShutDown
+                KeepAlive x -> do
+                  sleep x
+                  _ <- C.atomically $ commit c Start
+                  go ref
+            Reset -> do
+              a <- readIORef ref
+              when (not $ isNothing a) (cancel' ref)
+              _ <- start ref c
+              go ref
+            _ -> go ref
+    start ref c' = do
+      a' <- async (app >> C.atomically (commit c' Died))
+      writeIORef ref (Just a')
+      C.atomically $ commit c' Ready
+    cancel' ref = do
+      mapM_ cancel =<< readIORef ref
+      writeIORef ref Nothing
+
+runControlBox :: ControlConfig -> IO () -> IO ()
+runControlBox cfg action =
+  etc
+    ()
+    (Transducer $ \s -> s & S.takeWhile (/= ShutDown))
+    (boxForgetPlug (void <$> controlBox cfg action))
+
+-- | send Start, wait for a Ready signal, run action, wait x secs, then send Quit
+testBox :: IO Bool
+testBox = cb
+  where
+    action =
+      sequence_ $
+      (\x -> putStrLn x >> sleep 1) . (show :: Integer -> Text) <$>
+      reverse [0 .. 10]
+    cb = with consoleControlBox (controlBox (KeepAlive 3) action)
+  -- buff (bounded 1)
+  -- ControlStart
+  -- ControlReady
+  -- ControlQuit
+
+timeOut :: Double -> ControlBox m
+timeOut t =
+  Box <$> mempty <*> ((lift (sleep t) >> S.yield Stop) |> toEmit)
--- src/Box/IO.hs
+++ src/Box/IO.hs
@@ -43,38 +43,39 @@ import Box.Plugs
 import Box.Stream
 import Box.Transducer
 import Flow
-import Protolude hiding ((.), (<>))
+import Protolude hiding ((.), (<>), STM)
 import Streaming (Of(..), Stream)
 import qualified Streaming.Internal as S
 import qualified Streaming.Prelude as S
 import qualified Control.Monad.Conc.Class as C
 import qualified Control.Concurrent.Classy.CRef as C
+import Control.Monad.Conc.Class (STM)
 
 -- * console
 -- | a single stdin committer action
-cStdin_ :: Committer STM Text -> IO ()
+cStdin_ :: Committer (STM IO) Text -> IO ()
 cStdin_ c = do
   a <- getLine
   void $ atomically $ commit c a
 
 -- | a finite stdin committer action
-cStdin :: Int -> Committer STM Text -> IO ()
+cStdin :: Int -> Committer (STM IO) Text -> IO ()
 cStdin n c = replicateM_ n (cStdin_ c)
 
 -- | a forever stdin committer action
-cStdin' :: Committer STM Text -> IO ()
+cStdin' :: Committer (STM IO) Text -> IO ()
 cStdin' = forever . cStdin_
 
 -- | a Cont stdin emitter
-eStdin :: Int -> Cont IO (Emitter STM Text)
+eStdin :: Int -> Cont IO (Emitter (STM IO) Text)
 eStdin n = cStdin n |> emitPlug
 
 -- | read from console, throwing away read errors
-readStdin :: Read a => Cont IO (Emitter STM a)
+readStdin :: Read a => Cont IO (Emitter (STM IO) a)
 readStdin = emap (pure . either (const Nothing) Just) . eRead <$> eStdin 1000
 
 -- | a single stdout emitter action
-eStdout_ :: (Print a) => Emitter STM a -> IO ()
+eStdout_ :: (Print a) => Emitter (STM IO) a -> IO ()
 eStdout_ e = do
   a <- atomically $ emit e
   case a of
@@ -82,29 +83,29 @@ eStdout_ e = do
     Just a' -> putStrLn a'
 
 -- | a finite stdout emitter action
-eStdout :: (Print a) => Int -> Emitter STM a -> IO ()
+eStdout :: (Print a) => Int -> Emitter (STM IO) a -> IO ()
 eStdout n = replicateM_ n . eStdout_
 
 -- | a forever stdout emitter action
-eStdout' :: (Print a) => Emitter STM a -> IO ()
+eStdout' :: (Print a) => Emitter (STM IO) a -> IO ()
 eStdout' = forever . eStdout_
 
 -- | a Cont stdout committer
-cStdout :: Print a => Int -> Cont IO (Committer STM a)
+cStdout :: Print a => Int -> Cont IO (Committer (STM IO) a)
 cStdout n = eStdout n |> commitPlug
 
 -- | show to stdout
-showStdout :: Show a => Cont IO (Committer STM a)
-showStdout = contramap show <$> (cStdout 1000 :: Cont IO (Committer STM Text))
+showStdout :: Show a => Cont IO (Committer (STM IO) a)
+showStdout = contramap show <$> (cStdout 1000 :: Cont IO (Committer (STM IO) Text))
 
 -- | console box
 -- > etc () (Trans $ \s -> s & S.takeWhile (/="q") & S.map ("echo: " <>)) (console 5)
-consolePlug :: Int -> Cont IO (Box STM Text Text)
+consolePlug :: Int -> Cont IO (Box (STM IO) Text Text)
 consolePlug n = boxPlug (eStdout n) (cStdin n)
 
 -- * file operations
 -- | emit lines from a file
-emitLines :: FilePath -> Cont IO (Emitter STM Text)
+emitLines :: FilePath -> Cont IO (Emitter (STM IO) Text)
 emitLines filePath = Cont (withFile filePath ReadMode) >>= fromHandle .> toEmit
   where
     fromHandle :: Handle -> Stream (Of Text) IO ()
@@ -114,7 +115,7 @@ emitLines filePath = Cont (withFile filePath ReadMode) >>= fromHandle .> toEmit
         S.yield a
 
 -- | commit lines to a file
-commitLines :: FilePath -> Cont IO (Committer STM Text)
+commitLines :: FilePath -> Cont IO (Committer (STM IO) Text)
 commitLines filePath =
   Cont (withFile filePath WriteMode) >>= toHandle .> toCommit
   where
@@ -139,7 +140,7 @@ cCRef = do
   pure (ref, c, res)
 
 -- | commit to a monoidal CRef
-cCRefM :: (C.MonadConc m, Monoid a, Semigroup a) => m (C.CRef m a, Cont m (Committer (C.STM m) a), m a)
+cCRefM :: (C.MonadConc m, Monoid a) => m (C.CRef m a, Cont m (Committer (C.STM m) a), m a)
 cCRefM = do
   ref <- C.newCRef mempty
   let c = toCommitFold $
